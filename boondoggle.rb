3b05744c1ac3280c0d63f2ff753f994c0fd31b4e
gmorse81 <gmorse81@gmail.com>
2018-04-05 16:42:25 -0400

    Brew formula update for boondoggle version v0.1.4

--- boondoggle.rb
+++ boondoggle.rb
@@ -1,9 +1,9 @@
 class Boondoggle < Formula
   desc ""
   homepage ""
-  url "https://github.com/gmorse81/boondoggle/releases/download/v0.1.3/boondoggle_0.1.3_darwin_amd64.tar.gz"
-  version "0.1.3"
-  sha256 "ae8b9f5786e7bfbdf2c99124a02c98f99ed4ada540bb9c72e4c5c4fdb0ae8b71"
+  url "https://github.com/gmorse81/boondoggle/releases/download/v0.1.4/boondoggle_0.1.4_darwin_amd64.tar.gz"
+  version "0.1.4"
+  sha256 "b54979d12cc7d0ae98b2916433096608ad55e7b54e8c00951d5023ce0b8de9ba"
 
   def install
     bin.install "boondoggle"
