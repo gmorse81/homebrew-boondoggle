f9216cd20b193351df1577b9a458d7b5a40dca73
gmorse81 <gmorse81@gmail.com>
2020-09-10 10:30:59 -0400

    Brew formula update for boondoggle version v1.0.33

--- boondoggle.rb
+++ boondoggle.rb
@@ -2,16 +2,16 @@
 class Boondoggle < Formula
   desc ""
   homepage "https://github.com/gmorse81/boondoggle"
-  version "1.0.32"
+  version "1.0.33"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gmorse81/boondoggle/releases/download/v1.0.32/boondoggle_1.0.32_MacOS_x86_64.tar.gz"
-    sha256 "bc708e55686c569963c30f15a668e78b27418380f9dc83bb3417c75ab6bf9d63"
+    url "https://github.com/gmorse81/boondoggle/releases/download/v1.0.33/boondoggle_1.0.33_MacOS_x86_64.tar.gz"
+    sha256 "47dc17b2382f45df711468b1594e7c8ba57ee13900c9dcffceb47ab66a234a0e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gmorse81/boondoggle/releases/download/v1.0.32/boondoggle_1.0.32_Linux_x86_64.tar.gz"
-      sha256 "9b213cc9480db684988e47b249c1c88042256832417bc437638c921a38df5eee"
+      url "https://github.com/gmorse81/boondoggle/releases/download/v1.0.33/boondoggle_1.0.33_Linux_x86_64.tar.gz"
+      sha256 "30ee01fd13ffb50768484359f2de4a8b53e5773ac458c9ebd08bc1e06f3912d7"
     end
   end
 
